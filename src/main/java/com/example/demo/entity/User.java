93f86d6eccad2d7cf694e8c98a80770a4c47a55d
roryeiffe <46585645+roryeiffe@users.noreply.github.com>
2022-03-24 11:32:09 -0400

    Changed the name of the table
    so it doesn't conflict with the reserved SQL term 'user'.

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Data
 @ToString
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
